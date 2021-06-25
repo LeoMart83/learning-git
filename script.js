8a815f37ab5a40d0c338ec26dccf31dd714f1122
Leonid Martynov <lmart@softserveinc.com>
2021-06-25 13:09:08 +0300

    This was '2222' commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //document.body.innerHTML = '<h1>Hello from JS</h1>'
 console.log('Test 2323232323232323232323');
-<<<<<<< HEAD
 window.alert('How is your day?');
 
 console.log('new changes');
@@ -11,12 +10,9 @@ console.log('And third new changes');
 
 console.log('some new changes');
 //window.alert('How is your day?');
-=======
-//window.alert('How is your day?');
 
 const but = document.getElementById("23");
 
 but.addEventListener('click', () => console.log('23'));
 
 console.log(but);
->>>>>>> 6dea773 (new button feature)
