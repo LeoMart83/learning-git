92b7f31f6ff98bf7b5219e9f6a8d12a22fa91e00
Leonid Martynov <lmart@softserveinc.com>
2021-06-25 14:52:28 +0300

    Feature for second dev

--- script.js
+++ script.js
@@ -7,22 +7,57 @@ console.log('new changes');
 console.log('New new changes');
 
 console.log('And third new changes');
+<<<<<<< HEAD
+<<<<<<< HEAD
 
 console.log('some new changes');
 //window.alert('How is your day?');
 
 const but = document.getElementById("23");
 const span = document.getElementById('span');
+const rnd = document.getElementById('rnd');
+
+const colors = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let count = 0;
 
 const colors = [1,2,3];
 
-but.addEventListener('click', () => span.innerHTML = ++count);
-
+<<<<<<< HEAD
+<<<<<<< HEAD
 console.log('new lines for foo');
 <<<<<<< HEAD
 =======
 
 console.log('make background color gray!');
 >>>>>>> 55788c1 (new master changes)
+=======
+rnd.addEventListener('click', () => document.body.style.background = 'gray');
+<<<<<<< HEAD
+>>>>>>> 9ab71b4 (new button)
+=======
+
+console.log('test');
+
+console.log('test2');
+>>>>>>> e6d6204 (Added new lines)
+=======
+but.addEventListener('click', () => span.innerHTML = ++count);
+
+rnd.addEventListener('click', () => document.body.style.background = 'gray');
+<<<<<<< HEAD
+>>>>>>> 6f365bb (scr)
+=======
+
+console.log('git stash');
+>>>>>>> d3ff25e (patch)
+=======
+>>>>>>> 07aa467 (Deleted console.log)
+=======
+
+console.log('Hello From The Other Side');
+
+console.log('Tea');
+
+console.log('New cool feature');
+>>>>>>> 01b92a3 (Feature for second dev)
