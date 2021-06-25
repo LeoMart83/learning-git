6dea773ac2b9c2d90aff89d821a156ef5e4f99fe
Leonid Martynov <lmart@softserveinc.com>
2021-06-25 13:04:16 +0300

    new button feature

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.body.innerHTML = '<h1>Hello from JS</h1>'
+//document.body.innerHTML = '<h1>Hello from JS</h1>'
 console.log('Test 2323232323232323232323');
 //window.alert('How is your day?');
+
+const but = document.getElementById("23");
+
+but.addEventListener('click', () => console.log('23'));
+
+console.log(but);
