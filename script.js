f5e4c556f3040d9054fcfef394da5ebe6ece3136
LeoMart83 <mistikleo7@gmail.com>
2021-07-01 14:08:14 +0300

    Tea commit

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-//document.body.innerHTML = '<h1>Hello from JS</h1>'
-console.log('Test 2323232323232323232323');
-window.alert('How is your day?');
+console.log('definitely new code');
 
-console.log('new changes');
-
-console.log('New new changes');
-
-console.log('And third new changes');
-
-console.log('some new changes');
-
-const but = document.getElementById("23");
-const span = document.getElementById('span');
-
-let count = 0;
-
-but.addEventListener('click', () => span.innerHTML = ++count);
-
-console.log('new lines for foo');
+console.log('I adore tea');
