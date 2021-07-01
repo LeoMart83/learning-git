5e6cb2dc125ae7ebd3c6d41377968695cb6a4ca7
Leo Mart <mistikleo7@gmail.com>
2021-07-01 12:33:23 +0300

    My squashed commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.body.innerHTML = '<h1>Hello from JS</h1>'
 console.log('Test 2323232323232323232323');
 window.alert('How is your day?');
+
+console.log('new changes');
+
+console.log('New new changes');
+
+console.log('And third new changes');
