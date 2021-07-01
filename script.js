b2e0d82d3916f31b0a8e6dde7d483372d84e11ae
Leo Mart <mistikleo7@gmail.com>
2021-07-01 13:57:16 +0300

    Bizzare code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.body.innerHTML = '<h1>Hello from JS</h1>'
+//document.body.innerHTML = '<h1>Hello from JS</h1>'
 console.log('Test 2323232323232323232323');
+<<<<<<< HEAD
 window.alert('How is your day?');
 
 console.log('new changes');
@@ -10,3 +11,12 @@ console.log('And third new changes');
 
 console.log('some new changes');
 //window.alert('How is your day?');
+=======
+//window.alert('How is your day?');
+
+const but = document.getElementById("23");
+
+but.addEventListener('click', () => console.log('23'));
+
+console.log(but);
+>>>>>>> 6dea773 (new button feature)
